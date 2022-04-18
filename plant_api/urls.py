e0357d3e0d8dc6aaa89e8e1dd3b32d7abeb778ae
Angela Francisco <angelat.francisco@gmail.com>
2022-04-17 21:41:55 -0700

    updating url paths

--- plant_api/urls.py
+++ plant_api/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     # '/plants' will be routed to the PlantList view for handling
-    path('plants/', views.PlantList.as_view(), name='plant_list'),
+    path('api/plants/', views.PlantList.as_view(), name='plant_list'),
     # '/plants/id' will be routed to the PlantDetail view for handling
-    path('plants/<int:pk>/', views.PlantDetail.as_view(), name='plant_detail'),
-    path('status/', views.StatusList.as_view(), name='status_list'),
-    path('status/<int:pk>/', views.StatusDetail.as_view(), name='status_detail'),
+    path('api/plants/<int:pk>/', views.PlantDetail.as_view(), name='plant_detail'),
+    path('api/status/', views.StatusList.as_view(), name='status_list'),
+    path('api/status/<int:pk>/', views.StatusDetail.as_view(), name='status_detail'),
     # path('tasks/', views.TaskList.as_view(), name='task_list'),
     # path('tasks/<int:pk>/', views.TaskDetail.as_view(), name='task_detail'),
 ]
